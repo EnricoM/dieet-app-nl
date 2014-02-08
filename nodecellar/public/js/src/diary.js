f019ca222943b6e4766b01e14a3f5f0be2c19eef
EnricoM <enricominnaar@gmail.com>
2014-02-08 10:59:09 +0100

    bug fixes: resized some images, fixed some json errors, threw soome code away

--- nodecellar/public/js/src/diary.js
+++ nodecellar/public/js/src/diary.js
@@ -9,8 +9,6 @@ requirejs.config({
 require(["require_json_template", "bootstrap", "../src/serviceAgent", "jquery", "jquery.validate", "jquery.validate.override"], function(jsontemplate, bootstrap, serviceAgent, $) {
 	$(document).ready(function(){
 	
-		var modalStuff = "<div class='modal-backdrop in'></div>";
-		
 		var searchResultScreenNonXS =	
 			"{.section products}" + 
 			"<div class='row'>"+
@@ -37,7 +35,7 @@ require(["require_json_template", "bootstrap", "../src/serviceAgent", "jquery", 
 			"{.section products}" + 
 			"<form class='productDetailsContent' id='detailsId' detailsId={_id} >"+
 			"<fieldset>"+
-			"<input type='number' id='quantityInput'/>"+
+			"<input type='number' id='quantityInput' min='1' max='9999'/>"+
 			"<select name='Eenheden' id='unitsDiary'>"+
 			"{.repeated section Eenheden}" + 
 			"<option value={hoeveelheid}>{omschrijving}</option>"+
@@ -62,7 +60,7 @@ require(["require_json_template", "bootstrap", "../src/serviceAgent", "jquery", 
 					"{.section actuals}" +
 					"<tr>" +
 						"<th>{type}</th>" +
-						"<th class='tnr'>{totalKcal}</th>" +							
+						"<th id='totalKcal' class='tnr'>{totalKcal}</th>" +							
 						"<th class='tnr'>{totalProtein}</th>" +
 						"<th class='tnr'>{totalCarbon}</th>" +
 						"<th class='tnr'>{totalFat}</th>" +
@@ -110,6 +108,25 @@ require(["require_json_template", "bootstrap", "../src/serviceAgent", "jquery", 
 			getDiary(diaryDate);
 		});
 		
+		$("#clearInput").bind('click', function(event) {
+			clearInput();
+		});
+		
+		$("#showTable").bind('click', function(event) {
+			var containsTable = $("#tableResults").hasClass("hide").toString();
+			if ($("#tableResults").hasClass("hidden")) {
+				$("#tableResults").removeClass("hidden");
+			} else {
+				$("#tableResults").addClass("hidden")
+			}
+		});
+		
+		function clearInput() {
+			var category = $("#categories").val(-1);
+			var product = $("#product").val("");
+			$("#resultsNonXS").remove();
+		};
+		
 		function removeFromDiary(_id) {
 			var diaries = {
 				"diaryDate" : $('#diaryDatePicker').val(),
@@ -129,16 +146,19 @@ require(["require_json_template", "bootstrap", "../src/serviceAgent", "jquery", 
 		
 		function getDiary(diaryDate) {
 			var url = "/diaries?diaryDate=" + diaryDate;
+			$('#diaryDetails').modal('hide');
 			serviceAgent.doGet("GET", url, "json", "application/json; charset=utf-8", function(response) {
 				if (response && response.messages && response.messages[0]) {
 					var messageType = response.messages[0].messageType;
-					console.log('received some diary ', messageType);
 					switch(messageType) {
 						case "SUCCESS" : {
 							$('#dailyResultsTableNonXS').remove();
+							clearInput();
 							var _t4 = jsontemplate.Template(tableContentNonXS);
 							var fragment4 = _t4.expand(response);
 							$('#tableResults').append(fragment4);
+							var totalKcal = $("#totalKcal").text();
+							$("#totalKcalCopy").val("Totaal kCal: " + totalKcal);
 							break;
 						}
 						case "ERROR" : {
@@ -167,18 +187,6 @@ require(["require_json_template", "bootstrap", "../src/serviceAgent", "jquery", 
 		};
 		
 		function modalHandlers() {
-/*		
-			$('#closeModal').bind('click', function(event) {
-				$('.productDetailsHeader').remove();
-				$('.productDetailsContent').remove();
-				$('#diaryDetails').attr('aria-hidden', 'true');
-				$('#diaryDetails').removeClass('in');
-				$('#diaryDetails').css('display', 'none');
-				$('body').removeClass('modal-open');
-				$('.modal-backdrop').remove();
-				console.log('fdgfdgfdgfd');
-			});
-*/			
 			$('#addToDiary').bind('click', function(event) {
 				var _id = $('#detailsId').attr('detailsId');
 				var unit = $("#unitsDiary").val();
@@ -192,35 +200,7 @@ require(["require_json_template", "bootstrap", "../src/serviceAgent", "jquery", 
 				};
 				serviceAgent.doSubmit("POST", "/diaries", "json", "application/json; charset=utf-8", JSON.stringify(diaries), function(response) {
 					if (response && response.messages && response.messages[0] && response.messages[0].messageType === "SUCCESS") {
-						serviceAgent.doGet("GET", url, "json", "application/json; charset=utf-8", function(response) {
-							$('#diaryDetails').modal('hide');
-							if (response && response.messages && response.messages[0]) {
-								var messageType = response.messages[0].messageType;
-								switch(messageType) {
-									case "SUCCESS" : {
-										$('#dailyResultsTableNonXS').remove();
-										$("#resultsNonXS").remove();
-										var _t4 = jsontemplate.Template(tableContentNonXS);
-										var fragment4 = _t4.expand(response);
-										$('#tableResults').append(fragment4);
-										$('body').append('<div></div>');
-										break;
-									}
-									case "ERROR" : {
-										$(".alert").removeClass("hide").addClass("alert-error");
-										$("#messageText").text(response.messages[0].messageText);
-										$("#messageType").text(response.messages[0].messageType);
-										$('.alert').attr('title', response.messages[0].messageCode);																						
-										break;
-									}
-								}
-							} else {
-								$(".alert").removeClass("hide").addClass("alert-danger");
-								$("#messageText").text(response.messages[0].messageText);
-								$("#messageType").text(response.messages[0].messageType);
-								$('.alert').attr('title', response.messages[0].messageCode);												
-							}
-						});
+						getDiary(diaryDate);
 					}
 				});
 			});
@@ -241,6 +221,7 @@ require(["require_json_template", "bootstrap", "../src/serviceAgent", "jquery", 
 						var fragment2 = _t2.expand(response);
 						$('.modal-header').append(fragment1);
 						$('.modal-body').append(fragment2);
+						$('#addToDiary').focus();
 						modalHandlers();
 					}
 				});
