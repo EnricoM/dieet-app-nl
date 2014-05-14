59e98396e9f360f218626b91858698b87b408d35
EnricoM <enricominnaar@gmail.com>
2014-05-14 15:25:14 +0200

    working version
    working version??

--- nodecellar/server.js
+++ nodecellar/server.js
@@ -8,10 +8,20 @@ var Config = require('./routes/config'),
 MongoClient.connect('mongodb://localhost:27017/dieet', function(err, db) {
 
 	var app = express();
+
+	var allowCrossDomain = function(req, res, next) {
+		res.header('Access-Control-Allow-Origin', 'http://localhost:8000');
+		res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+		res.header('Access-Control-Allow-Headers', 'Content-Type');
+		res.header('Access-Control-Allow-Credentials', 'true');
+		next();
+	}
+	
 	app.set('port', conf.PORT || 7396);
 	app.set('env', process.env.NODE_ENV || 'development');
 
 	app.use(express.favicon('./favicon.ico')); 
+	app.use(allowCrossDomain);
 	app.use(express.static(path.join(__dirname, 'public')));
 	app.use(express.logger('dev'));
 
